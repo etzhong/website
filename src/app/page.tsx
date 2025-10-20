63709b8a0fcc126e1abb0b0767f532215f2f76ef
etzhong <eric@erictzhong.com>
2025-10-20 03:01:00 -0700

    Website content update 10.20.25

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -30,18 +30,23 @@ export default function Home() {
 
         <div className="text-gray-700 dark:text-gray-300">
           <p className="mt-4">
-            I&apos;m an engineer, developer, and part-time creative finishing
-            up their senior year in Electrical Engineering and Computer Science
-            at the University of Southern California (
-            <a
-              href="https://viterbischool.usc.edu"
-              target="_blank"
+            I&apos;m an engineer, developer, and part-time creative based in
+            the Bay Area&mdash;specializing in hardware and low-level systems.
+            I&apos;m currently working as a GPU Architect at&nbsp;
+            <a href="https://www.nvidia.com/en-us" target="_blank"
               className="border-b inline-block"
             >
-            USC
+            NVIDIA
             </a>
-            ).
+            &nbsp;in Santa Clara. Before that, I graduated concurrently with
+            my master&apos;s and bachelor&apos;s degrees in Electrical
+            Engineering and Computer Science from&nbsp;
+            <a href="https://viterbischool.usc.edu" target="_blank"
+              className="border-b inline-block">
+            USC
+            </a>.
           </p>
+
 
           <p className="mt-4">
             Born and raised in the Philly suburbs (Go Birds!), I originally
@@ -53,53 +58,35 @@ export default function Home() {
           </p>
 
           <p className="mt-4 mb-4">
-            In the past couple years I&apos;ve worked in a variety of
-            areas, including&nbsp;
-            <a
-                href="https://www.nvidia.com/en-us"
-                target="_blank"
-                className="border-b inline-block"
-            >
-              GPU hardware engineering
-            </a>
-            ,&nbsp;
-            <a
-                href="https://arxiv.org/abs/2401.18050"
-                target="_blank"
-                className="border-b inline-block"
-            >
-              photonic computing research
-            </a>
-            , and&nbsp;
-            <a
-                href="https://usclavalab.org"
-                target="_blank"
-                className="border-b inline-block"
-            >
-              startup development
-            </a>
-            . Currently I&apos;m interested in leveraging hardware acceleration
-            and high performance computing towards solving uniquely challenging
-            problems, along with learning more about agile hardware/software
-            co-design.
+            Over the past several years, I&apos;ve worked across a variety of
+            areas&mdash;including GPU hardware design, FPGA engineering,
+            low-level software development, photonic computing research, and
+            entrepreneurship. Currently I&apos;m interested in hardware
+            acceleration, reconfigurable computing (especially FPGAs),
+            open-source silicon, and hardware/software co-design.
           </p>
-          
+
           <p className="mt-4 mb-4">
-            Outside of engineering, you can find me reading & writing, playing
-            poker, going down some internet rabbit hole, or trying out food
-            spots around LA.
+            Outside of engineering, you can find me reading and writing, doing
+            archery (I shoot recurve!), spending time outdoors, playing poker,
+            or trying out new food spots around the Bay.
           </p>
 
           <p className="mb-4">
-            If you&apos;d like to get in touch, feel free&nbsp;
+            If you&apos;d like to get in touch, feel free to&nbsp;
             <a
               href="mailto:eric@erictzhong.com"
               className="border-b inline-block"
             >
               shoot me an email
             </a>
-            &nbsp;or reach out on any of my socials. Cheers!
+            &nbsp;or reach out on any of my socials.
           </p>
+
+          <p className="mb-4">
+          Cheers!
+          </p>
+
         </div>
 
         <div className="flex space-x-4 mb-2 mt-4">
